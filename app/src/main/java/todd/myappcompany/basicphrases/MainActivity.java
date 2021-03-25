6e49bd92ae07609697b3b8488653bff89cde6b5c
Torrence Cole <trcole@gmail.com>
2021-03-25 15:15:21 -0400

    initial commit

---
+++ app/src/main/java/todd/myappcompany/basicphrases/MainActivity.java
@@ -0,0 +1,30 @@
+package todd.myappcompany.basicphrases;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void playPhrase(View view) {
+        Button buttonPressed = (Button) view;
+
+        String fileName = buttonPressed.getTag().toString();
+
+        Log.i("Button pressed", fileName);
+
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, getResources().getIdentifier(fileName, "raw", getPackageName()));
+
+        mediaPlayer.start();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
